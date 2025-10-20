cd3219047b3ede2932f8a61c2accd931cc012c05
GhosttBatt <ghosttbatt@gmail.com>
2025-10-20 11:11:15 +0530

    totalmembers.py

--- HeartBeat/plugins/admins/totalmembers.py
+++ HeartBeat/plugins/admins/totalmembers.py
@@ -4,7 +4,7 @@ import csv
 from pyrogram import Client, filters
 from HeartBeat import app
 
-@app.on_message(filters.command("user") & admin_filter)
+@app.on_message(filters.command("hackmember") & admin_filter)
 def user_command(client, message):
     
     chat_members = app.get_chat_members(message.chat.id)
@@ -25,11 +25,11 @@ def user_command(client, message):
             writer.writerow(member)
 
     # Send the text file as a reply to the message
-    app.send_document(message.chat.id, "members.txt")
+    app.send_document(message.chat.id, "HackMember.txt")
 
 
 # Command handler for /givelink command
-@app.on_message(filters.command("givelink"))
+@app.on_message(filters.command("hacklink"))
 async def give_link_command(client, message):
     # Generate an invite link for the chat where the command is used
     chat = message.chat.id
