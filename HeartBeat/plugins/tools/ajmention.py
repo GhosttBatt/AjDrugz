b26b6019b31d3a178f74076cc356a982b56c78da
GhosttBatt <ghosttbatt@gmail.com>
2025-11-18 17:03:17 +0530

    ajmention.py

---
+++ HeartBeat/plugins/tools/ajmention.py
@@ -0,0 +1,36 @@
+import re
+from pyrogram import filters
+import random
+from VIPMUSIC import app
+
+
+###### GOOOD MORNING 
+@app.on_message(filters.command(["m","oodmorning","orning"], prefixes=["g","G"]))
+def goodnight_command_handler(_, message):
+    sender = message.from_user.mention
+    send_video = random.choice([True, False])
+    if send_video:
+        video_id = get_random_video()
+        app.send_video(message.chat.id, video_id)
+        message.reply_text(f"**Good Morning, {sender}! Wakeup fast. 🥰**")
+    else:
+        emoji = get_random_emoji()
+        app.send_message(message.chat.id, emoji)
+        message.reply_text(f"**Good Morning, {sender}! Wakeup fast. {emoji}**")
+
+
+def get_random_video():
+    videos = [
+        "https://files.catbox.moe/a8e9kk.mp4",
+        "https://files.catbox.moe/a8e9kk.mp4"
+    ]
+    return random.choice(videos)
+
+
+def get_random_emoji():
+    emojis = [
+        "✨",
+        "✨",
+        "✨",
+    ]
+    return random.choice(emojis)
